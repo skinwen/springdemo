ed9094dd0fa0ed31a80565de049b90f40a8a8c5d
skinwen <skinwen@gmail.com>
2019-01-24 22:45:15 +0800

    +add

--- sql/wt_ddl.sql
+++ sql/wt_ddl.sql
@@ -8,10 +8,10 @@ CREATE TABLE user_info(
   INDEX idx_n1(mobile_no)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
+
 CREATE TABLE subject(
   id BIGINT(10) AUTO_INCREMENT PRIMARY KEY COMMENT '主键',
-  subject_desc VARCHAR(100) COMMENT '主题描述',
-  subject_item_desc varchar(20) COMMENT '子话题描述',
+  subject_name VARCHAR(100) COMMENT '主题描述',
   crt_dat DATETIME DEFAULT current_timestamp COMMENT '创建时间',
   upt_dat DATETIME DEFAULT current_timestamp COMMENT '更新时间'
 )ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
@@ -20,12 +20,34 @@ CREATE TABLE subject(
 CREATE TABLE subject_item(
   id BIGINT(10) AUTO_INCREMENT PRIMARY KEY COMMENT '主键',
   subject_id BIGINT(100) COMMENT '主题描述',
-  subject_item_desc varchar(20) COMMENT '子话题描述',
   subject_item_name VARCHAR(100) COMMENT '子话题',
-  item_theme VARCHAR(100) COMMENT '子话题主题',
-  item_content VARCHAR(500) COMMENT '子话题内容',
-  item_md5 VARCHAR(64) COMMENT '摘要',
   crt_dat DATETIME DEFAULT current_timestamp COMMENT '创建时间',
   upt_dat DATETIME DEFAULT current_timestamp COMMENT '更新时间',
   INDEX idx_n1(subject_id)
+)ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+CREATE TABLE subject_item_content(
+  id BIGINT(10) AUTO_INCREMENT PRIMARY KEY COMMENT '主键',
+  subject_item_id BIGINT(100) COMMENT '主题描述',
+  theme VARCHAR(100) COMMENT '主题',
+  content VARCHAR(500) COMMENT '内容',
+  md5 VARCHAR(64) COMMENT '摘要',
+  crt_dat DATETIME DEFAULT current_timestamp COMMENT '创建时间',
+  upt_dat DATETIME DEFAULT current_timestamp COMMENT '更新时间',
+  INDEX idx_n1(subject_item_id),
+  INDEX idx_n2(md5)
+)ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+DROP TABLE subject_screen;
+CREATE TABLE subject_screen(
+  id BIGINT AUTO_INCREMENT PRIMARY KEY COMMENT '主键',
+  theme VARCHAR(100) COMMENT '主题',
+  target VARCHAR(100) COMMENT '目的',
+  content VARCHAR(500) COMMENT '内容',
+  img_url VARCHAR(200) COMMENT '图片地址',
+  release_time DATETIME DEFAULT current_timestamp COMMENT '发布时间',
+  crt_dat DATETIME DEFAULT current_timestamp COMMENT '创建时间',
+  upt_dat DATETIME DEFAULT current_timestamp COMMENT '更新时间',
+  md5 VARCHAR(64) COMMENT '摘要',
+  type CHAR(1) COMMENT '类型，1：恋爱解析，2：聊天截图',
+  INDEX idx_n1(md5)
 )ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
